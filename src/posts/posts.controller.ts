06e2c3a0c67800dff665c370b9d01cb5a50e6d50
Rohit Jire <rohitjire55@gmail.com>
2021-03-16 13:40:30 +0530

    crud done

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,18 +1,13 @@
 import express = require('express')
 import Post from './post.interface'
+import postModel from './post.models'
 
 class PostController{
 
     public path = '/posts'
     public router = express.Router()
 
-    private posts: Post[] = [
-        {
-          author: 'Marcin',
-          content: 'Dolor sit amet',
-          title: 'Lorem Ipsum',
-        }
-      ];
+    private posts = postModel
 
     constructor(){
         this.intializeRoutes();
@@ -20,18 +15,65 @@ class PostController{
 
     public intializeRoutes(){
         this.router.get(this.path,this.getAllPosts)
+        this.router.get(`${this.path}/:id`, this.getPostById)
+        this.router.patch(`${this.path}/:id`,this.modifyPost)
+        this.router.delete(`${this.path}/:id`,this.deletePost)
         this.router.post(this.path,this.createAPost)
 
     }
-    getAllPosts = (request:express.Request, response: express.Response)=>{
-        response.send(this.posts)
+    private getAllPosts = (request:express.Request, response: express.Response)=>{
+      this.posts.find()
+      .then(posts =>{
+        response.send(posts)
+      })
     }
 
-    createAPost = (request: express.Request, response: express.Response) => {
-        const post: Post = request.body;
-        this.posts.push(post);
-        response.send(post);
+    private createAPost = (request: express.Request, response: express.Response) => {
+        const postData: Post = request.body;
+
+        const createdPost = new postModel(postData)
+        createdPost.save()
+        .then(savePost=>{
+          response.send(savePost)
+        })
       }
 
-}
+    private getPostById(request: express.Request, response: express.Response){
+      const id = request.params.id
+      this.posts.findById(id)
+      .then(post =>{
+        response.send(post)
+      })
+    }
+
+    private modifyPost(request: express.Request, response: express.Response){
+      const id = request.params.id
+      const postData: Post = request.body
+      this.posts.findByIdAndUpdate(id, postData, {new: true})
+      .then(
+        post=>{
+          response.send(post)
+        }
+      )     
+    }
+
+  private deletePost(request: express.Request, response: express.Response){
+    const id = request.params.id
+    this.posts.findByIdAndDelete(id)
+    .then(successResponse =>{
+      if(successResponse){
+        response.send(200)
+      }
+      else{
+        response.send(404)
+      }
+    })
+  }
+
+
+
+
+
+    }
+
 export default PostController
